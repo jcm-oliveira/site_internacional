df311b1947c13e48bbedf47ed2ed327ac5a43583
Joao Carlos <trompete007@gmail.com>
2022-03-11 17:57:38 -0300

    correções e ajustes

--- src/js/nav-menu.js
+++ src/js/nav-menu.js
@@ -3,7 +3,7 @@
 //DASHBOARD CARREGADO AUTOMATICAMENTE QUANDO O USUÁRIO LOGA NO SISTEMA
 //---------------------------------------------------------------------
 
-qualEndereco("INT0101_Dashboard")
+qualEndereco("home")
 
 //-----------------------------------------------------------
 //QUANDO UM BOTÃO É ACIONADO, ESTA FUNÇÃO CAPTURA O ID DO BOTÃO
@@ -24,7 +24,7 @@ function LoadButtonRoutine(){
 
 function LoadMenuOption(){
     
-    document.getElementById('grupo-menu').addEventListener('click', function(e){
+    document.getElementById('group-menu').addEventListener('click', function(e){
         
     qualEndereco(e.target.id)}, {once:true}) 
     
@@ -35,13 +35,16 @@ function LoadMenuOption(){
 //DE ACORDO COM O ID CAPTURADO NA FUNÇÃO "CarregarRotina" ATRIBUI O ENDEREÇO DA PÁGINA A SER ABERTA
 //-------------------------------------------------------------------------------------------------
 
-function qualEndereco(rotina){
+function qualEndereco(rotine){
+
+    console.log(rotine)
    
-    switch(rotina){
-        case 'INT0101_Dashboard': carregarTela("/views/Interface/INT0101_Dashboard.html"); break;
-        case 'PAR0100_Consult': carregarTela("/views/Parametrizacoes/AreasVida/PAR0100_Consult.html"); break;
-        case 'PAR0101_Insert': carregarTela("/views/Parametrizacoes/AreasVida/PAR0101_Insert.html"); break;
-        case 'PAR0102_Edit': carregarTela("//views/Parametrizacoes/AreasVida/PAR0102_Edit.html"); break;
+    switch(rotine){
+        case 'home': carregarTela("/src/views/home.html"); break;
+        case 'associates': carregarTela("/src/views/associates.html"); break;
+        case 'worship-services': carregarTela("/src/views/worship-services.html"); break;
+        case 'announcements': carregarTela("/src/views/announcements.html"); break;
+        case 'offers': carregarTela("/src/views/offers.html"); break;
         default: alert("Esta tela ainda não foi criada..."); break;
         }
 }
