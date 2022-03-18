aef5dd466776a91a937e203ca25b0b293f64b547
Joao Carlos <trompete007@gmail.com>
2022-03-18 15:13:58 -0300

    reorganização das páginas

--- src/js/nav-menu.js
+++ src/js/nav-menu.js
@@ -40,21 +40,21 @@ function qualEndereco(rotine){
     console.log(rotine)
    
     switch(rotine){
-        case 'home': carregarTela("/src/views/home.html"); break;
-        case 'associates': carregarTela("/src/views/associates.html"); break;
-        case 'worship-services': carregarTela("/src/views/worship-services.html"); break;
-        case 'announcements': carregarTela("/src/views/announcements.html"); break;
-        case 'offers': carregarTela("/src/views/offers.html"); break;
+        case 'home': carregarTela("/src/views/public-pages/home.html"); break;
+        case 'associates': carregarTela("/src/views/public-pages/associates.html"); break;
+        case 'worship-services': carregarTela("/src/views/public-pages/worship-services.html"); break;
+        case 'announcements': carregarTela("/src/views/public-pages/announcements.html"); break;
+        case 'offers': carregarTela("/src/views/public-pages/offers.html"); break;
         //About us
-            case 'the-beginning': carregarTela("/src/views/about-us/the-beginning.html"); break;
-            case 'organization': carregarTela("/src/views/about-us/organization.html"); break;
-            case 'statistics': carregarTela("/src/views/about-us/statistics.html"); break;
-            case 'our-history': carregarTela("/src/views/about-us/our-history.html"); break;
+            case 'the-beginning': carregarTela("/src/views/public-pages/about-us/the-beginning.html"); break;
+            case 'organization': carregarTela("/src/views/public-pages/about-us/organization.html"); break;
+            case 'statistics': carregarTela("/src/views/public-pages/about-us/statistics.html"); break;
+            case 'our-history': carregarTela("/src/views/public-pages/about-us/our-history.html"); break;
         //Our Mission
-            case 'administrative-principles': carregarTela("/src/views/our-mission/administrative-principles.html"); break;
-            case 'faith-doctrine': carregarTela("/src/views/our-mission/faith-doctrine.html"); break;
-            case 'international-conventions': carregarTela("/src/views/our-mission/international-conventions.html"); break;
-            case 'teachings': carregarTela("/src/views/our-mission/teachings.html"); break;
+            case 'administrative-principles': carregarTela("/src/views/public-pages/our-mission/administrative-principles.html"); break;
+            case 'faith-doctrine': carregarTela("/src/views/public-pages/our-mission/faith-doctrine.html"); break;
+            case 'international-conventions': carregarTela("/src/views/public-pages/our-mission/international-conventions.html"); break;
+            case 'teachings': carregarTela("/src/views/public-pages/our-mission/teachings.html"); break;
 
         default: alert("Esta tela ainda não foi criada..."); break;
         }
