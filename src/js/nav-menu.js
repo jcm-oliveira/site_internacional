ba263daf5a17e01b30e2cc68e65f9c56ec2c5170
Joao Carlos <trompete007@gmail.com>
2022-04-01 18:19:16 -0300

    avisos para mobile

--- src/js/nav-menu.js
+++ src/js/nav-menu.js
@@ -3,7 +3,7 @@
 //DASHBOARD CARREGADO AUTOMATICAMENTE QUANDO O USUÁRIO LOGA NO SISTEMA
 //---------------------------------------------------------------------
 
-qualEndereco("worship-services")
+qualEndereco("announcements")
 
 //-----------------------------------------------------------
 //QUANDO UM BOTÃO É ACIONADO, ESTA FUNÇÃO CAPTURA O ID DO BOTÃO
